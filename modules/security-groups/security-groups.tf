fd72cdc854b911b3b654b188bbbf1426bf9a35a7
lhes23 <32542078+lhes23@users.noreply.github.com>
2024-12-19 22:44:16 +0800

    add port 443

--- modules/security-groups/security-groups.tf
+++ modules/security-groups/security-groups.tf
@@ -1,4 +1,3 @@
-
 # Security Group for ALB and EC2
 resource "aws_security_group" "lb_sg" {
   name   = "lb-sg"
@@ -7,6 +6,13 @@ resource "aws_security_group" "lb_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
