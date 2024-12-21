0235bbf0af6bfa420b01d93b429a93d9e8a0046c
lhes23 <32542078+lhes23@users.noreply.github.com>
2024-12-21 17:14:37 +0800

    delete staging

--- environments/staging/userdata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install -y nginx
-sudo systemctl start nginx
-echo "<h1>This webserver IP: $(hostname -i)</h1><h2>SP-API</h2>" > /var/www/html/index.nginx-debian.html
